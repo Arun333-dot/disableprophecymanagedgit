aaed5630aba78cdb5b5d9a785cc6aa9cf1fefaa4
arunsharma@prophecy.io <arunsharma@prophecy.io>
2024-12-26 07:02:12 +0000

    Simplifies data structure in `ewdfrgthyuj` model by selecting specific fields from `databricks_large`

--- sxcdv/models/ewdfrgthyuj.sql
+++ sxcdv/models/ewdfrgthyuj.sql
@@ -4,8 +4,19 @@ WITH databricks_large AS (
   
   FROM {{ source('hive_metastore.rohit', 'databricks_large') }}
 
+),
+
+FlattenSchema_1 AS (
+
+  {#Simplifies the data structure by selecting specific fields from a large dataset.#}
+  SELECT 
+    id AS id,
+    department AS department
+  
+  FROM databricks_large AS in0
+
 )
 
 SELECT *
 
-FROM databricks_large
+FROM FlattenSchema_1
